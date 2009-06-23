c2e6b1e8f6414f517b5f13be8e09c425be25ac8c
Duncan Coutts <duncan@haskell.org>
2009-06-23 01:17:24 +0000

    Rearrange code in the main test module
    And add type sigs.

--- test/Test.hs
+++ test/Test.hs
@@ -5,28 +5,27 @@ import Codec.Compression.Zlib.Internal
 import Test.Codec.Compression.Zlib.Internal
 import Test.Codec.Compression.Zlib.Stream
 
-import Control.Monad
-import Data.Word
 import Test.QuickCheck
-import qualified Data.ByteString.Lazy as BL
-
 import Test.Framework
 import Test.Framework.Providers.QuickCheck2
 
-instance Arbitrary Word8 where
-  arbitrary = fmap fromInteger arbitrary
-  shrink = map fromInteger . shrink . toInteger
+import Control.Monad
+import Data.Word
+import qualified Data.ByteString.Lazy as BL
 
-maxStrSize = 10000
 
--- convert a QC size parameter into one for generating long lists,
--- growing inverse exponentially up to maxStrSize
-strSize n = floor (maxStrSize * (1 - 2 ** (-fromIntegral n/100)))
+main :: IO ()
+main = defaultMain [
+    testProperty "decompress . compress = id (standard)"           prop_decompress_after_compress,
+    testProperty "decompress . compress = id (Zlib -> GZipOrZLib)" prop_gziporzlib1,
+    testProperty "decompress . compress = id (GZip -> GZipOrZlib)" prop_gziporzlib2
+  ]
 
-instance Arbitrary BL.ByteString where
-  arbitrary = sized $ \sz -> resize (strSize sz) $ fmap BL.pack $ listOf $ arbitrary
-  shrink = map BL.pack . shrink . BL.unpack
 
+prop_decompress_after_compress :: Format
+                               -> CompressParams
+                               -> DecompressParams
+                               -> Property
 prop_decompress_after_compress w cp dp =
    (w /= zlibFormat || decompressWindowBits dp >= compressWindowBits cp) &&
    -- Zlib decompression has been observed to fail with both compress and decompress
@@ -36,17 +35,39 @@ prop_decompress_after_compress w cp dp =
    decompressBufferSize dp > 0 && compressBufferSize cp > 0 ==>
    liftM2 (==) (decompress w dp . compress w cp) id
 
+
+prop_gziporzlib1 :: CompressParams
+                 -> DecompressParams
+                 -> Property
 prop_gziporzlib1 cp dp =
    decompressWindowBits dp > compressWindowBits cp &&
    decompressBufferSize dp > 0 && compressBufferSize cp > 0 ==>
    liftM2 (==) (decompress gzipOrZlibFormat dp . compress zlibFormat cp) id
 
+
+prop_gziporzlib2 :: CompressParams
+                 -> DecompressParams
+                 -> Property
 prop_gziporzlib2 cp dp =
    decompressBufferSize dp > 0 && compressBufferSize cp > 0 ==>
    liftM2 (==) (decompress gzipOrZlibFormat dp . compress gzipFormat cp) id
 
-main = defaultMain [
-         testProperty "decompress . compress = id (standard)" prop_decompress_after_compress,
-         testProperty "decompress . compress = id (Zlib -> GZipOrZLib)" prop_gziporzlib1,
-         testProperty "decompress . compress = id (GZip -> GZipOrZlib)" prop_gziporzlib2
-       ]
+
+-------------------
+-- QuickCheck Utils
+
+instance Arbitrary Word8 where
+  arbitrary = fmap fromInteger arbitrary
+  shrink = map fromInteger . shrink . toInteger
+
+maxStrSize :: Double
+maxStrSize = 10000
+
+-- convert a QC size parameter into one for generating long lists,
+-- growing inverse exponentially up to maxStrSize
+strSize :: Int -> Int
+strSize n = floor (maxStrSize * (1 - 2 ** (-fromIntegral n/100)))
+
+instance Arbitrary BL.ByteString where
+  arbitrary = sized $ \sz -> resize (strSize sz) $ fmap BL.pack $ listOf $ arbitrary
+  shrink = map BL.pack . shrink . BL.unpack
