16a22ad29aa18c50b182736b99d24d2b33b33bf7
Duncan Coutts <duncan@community.haskell.org>
2011-12-29 18:04:50 +0000

    Make the example gzip prog use the max compression ratio

--- examples/gzip.hs
+++ examples/gzip.hs
@@ -3,4 +3,6 @@ module Main where
 import qualified Data.ByteString.Lazy as B
 import qualified Codec.Compression.GZip as GZip
 
-main = B.interact GZip.compress
+main = B.interact $ GZip.compressWith GZip.defaultCompressParams {
+                      GZip.compressLevel = GZip.BestCompression
+                    }
