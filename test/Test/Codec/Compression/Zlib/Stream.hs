23d6a912d894994951a1050f3168c22c3c588142
Duncan Coutts <duncan@community.haskell.org>
2015-04-19 19:08:36 +0000

    Fix testsuite
    Remove now duplicate instances.

--- test/Test/Codec/Compression/Zlib/Stream.hs
+++ test/Test/Codec/Compression/Zlib/Stream.hs
@@ -9,41 +9,34 @@ import Codec.Compression.Zlib.Internal
 import Test.QuickCheck
 
 
-deriving instance Show Format
 instance Arbitrary Format where
   -- GZipOrZlib omitted since it's not symmetric
   arbitrary = elements [gzipFormat, zlibFormat, rawFormat]
 
 
-deriving instance Show Method
 instance Arbitrary Method where
    arbitrary = return deflateMethod
 
 
-deriving instance Show CompressionLevel
 instance Arbitrary CompressionLevel where
   arbitrary = elements $ [defaultCompression, noCompression,
                           bestCompression, bestSpeed]
                       ++ map compressionLevel [1..9]
 
 
-deriving instance Eq WindowBits
 deriving instance Ord WindowBits
-deriving instance Show WindowBits
 instance Arbitrary WindowBits where
   arbitrary = elements $ defaultWindowBits:map windowBits [8..15]
   shrink DefaultWindowBits = []
   shrink (WindowBits n) = defaultWindowBits:map windowBits [n+1..15]
 
 
-deriving instance Show MemoryLevel
 instance Arbitrary MemoryLevel where
   arbitrary = elements $ [defaultMemoryLevel, minMemoryLevel, maxMemoryLevel]
                       ++ [memoryLevel n | n <- [1..9]]
 
 
 
-deriving instance Show CompressionStrategy
 instance Arbitrary CompressionStrategy where
   arbitrary = elements $ [defaultStrategy, filteredStrategy, huffmanOnlyStrategy]
                    -- These are disabled by default in the package
