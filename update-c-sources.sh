156f5c03cfa165a98701d946a930281b5e37308b
Bodigrim <andrew.lelechenko@gmail.com>
2022-03-30 21:35:20 +0100

    Do not use rg in update-c-sources.sh

--- update-c-sources.sh
+++ update-c-sources.sh
@@ -1,13 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e pipefail
 
-zlib="$(curl http://zlib.net/ | rg '(zlib-\d+(\.\d+)*)\.tar\.gz' -o -r '$1' | head -n1)"
+curl -s http://zlib.net/zlib.tar.gz | tar xz
 
-curl "http://zlib.net/${zlib}.tar.gz" -o "${zlib}.tar.gz"
-tar xvf "${zlib}.tar.gz"
-
-for csrc in $(ls -1 cbits)
+for csrc in cbits/*
 do
-    cp "${zlib}/${csrc}" cbits/
+    mv zlib-*/"${csrc##*/}" cbits/
 done
+
+rm -rf zlib-*
